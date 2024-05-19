f5ec77de12cd43668b301d4ca2e92e28dbb8d7a4
김주환 <kjh71047@naver.com>
2024-05-19 20:45:05 +0900

    스프링 초기설정

---
+++ src/main/java/moodon/moodonApplication.java
@@ -0,0 +1,13 @@
+package moodon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class moodonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(moodonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/moodon/MoodOnApplicationTests.java
@@ -0,0 +1,13 @@
+package moodon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoodOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
